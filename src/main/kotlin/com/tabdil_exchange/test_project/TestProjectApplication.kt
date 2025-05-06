e25b6005c3e802fe447e3a4c4d18d5e45bc614e4
soroush.esk <semp1381@gmail.com>
2025-05-06 17:55:01 +0330

    initial project

---
+++ src/main/kotlin/com/tabdil_exchange/test_project/TestProjectApplication.kt
@@ -0,0 +1,11 @@
+package com.tabdil_exchange.test_project
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TestProjectApplication
+
+fun main(args: Array<String>) {
+	runApplication<TestProjectApplication>(*args)
+}
